0ff5ca7abdef8102b2f656daf00be9da21e27ffa
Alexander <losev.pavlovo152@gmail.com>
2024-08-25 16:37:41 +0300

    submit

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,14 +1,27 @@
 import { useState } from "react";
 
 export default function Form() {
-  const [name, setName] = useState("");
+  const [name, setName] = useState({ firstName: "", lastName: "" });
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(name);
+  }
   return (
     <form>
+      <h2>
+        {name.firstName} - {name.lastName}
+      </h2>
       <input
-        onChange={(e) => setName(e.target.value)}
+        onChange={(e) => setName({ ...name, firstName: e.target.value })}
         type="text"
-        value={name}
+        value={name.firstName}
       />
+      <input
+        type="text"
+        onChange={(e) => setName({ ...name, lastName: e.target.value })}
+        value={name.lastName}
+      />
+      <button onClick={(e) => handleSubmit(e)}>add</button>
     </form>
   );
 }
